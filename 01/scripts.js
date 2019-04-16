a5cab9596a882982e7a1d248ec28523ba250400b
Afanasyeva Polina <NotBadPolly@yandex.ru>
2019-04-16 10:44:26 +0300

    new version

--- 01/scripts.js
+++ 01/scripts.js
@@ -5,24 +5,26 @@ const progress = document.querySelector('.progress')
 const WORK_TIME = 2000
 
 function startTimer(e) {
-  // По умолчанию при нажатии на Ввод форма прегружает страницу
-  // Ниже мы говорим форме не делать действия по умолчанию
+  // По умолчанию при нажатии на «Ввод» форма перезагружает страницу
+  // Ниже мы говорим форме не делай действия по умолчанию
   e.preventDefault()
 
-  // Мы слушаем события (onclick, onsubmit, onfocus и т.д) на элементах
-  // Таргет – это тот объект, на котором произошло событие (е)
-  // form.onsubmit = startTimer - вот тут мы вешаем событие на форму
+  // Мы слушаем события (onclick, onsubmit, onfocus и т.д.) на элементах
+  // target — это тот объект, на котором произошло событие (e)
+  // form.onsubmit = startTimer — вот тут мы «вешаем» событие на форму
   // Мы знаем, что внутри формы лежит инпут, а событие submit происходит у формы
   // Поэтому мы получаем доступ к инпуту через событие и форму
   const input = e.target.querySelector('input')
-  // Добавили атрибут дисейблед внутрь html tag инпута
+  // Добавили атрибут disabled внутрь HTML-тега инпута
   // Это заблокировало возможность писать и поменяло цвет
   input.disabled = true
 
-  // Создаем ключ, чтобы отличать друг от друга заверешенные задачи
+  // Создаем ключ, чтобы отличать друг от друга завершенные задачи
   // В будущем мы сможем находить и удалять записи при помощи ключа
+  // Date.now() генирирует https://ru.wikipedia.org/wiki/Unix-%D0%B2%D1%80%D0%B5%D0%BC%D1%8F
   const key = Date.now()
   // Записываем в память браузера новый элемент
+  // https://tproger.ru/articles/localstorage/
   localStorage.setItem(key, input.value)
 
   progress.style.width = '100vw'
@@ -39,18 +41,28 @@ function startTimer(e) {
 
     const listItem = document.createElement('li')
     listItem.innerText = input.value
+    // tasks — это <ul></ul>
+    // тут мы добавляем в список новый элемент
     tasks.appendChild(listItem)
 
+    // Сбрасываем значение инпута на пустое
     input.value = ''
   }, WORK_TIME)
 }
 
 function loadHistory() {
+  // Узнаем размер занятой памяти
   const historySize = localStorage.length
 
+  // Если память не пустая, то есть размер больше нуля
   if (historySize > 0) {
-    for (var i = 0; i < historySize; i++) {
+    // Это обычный цикл со счетчиком i
+    // Он работает, пока счетчик меньше размера памяти
+    // Тем самым мы проходим по всем эелментами в памяти
+    for (let i = 0; i < historySize; i++) {
+      // Достали элемент из памяти
       const key = localStorage.key(i)
+      // Достали имя элемента, чтобы ниже воспользоваться им
       const taskName = localStorage.getItem(key)
 
       const listItem = document.createElement('li')
