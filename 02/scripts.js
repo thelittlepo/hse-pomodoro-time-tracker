6ac6e911bdcccdeb73508668db40e5d35ed1cb7e
Afanasyeva Polina <NotBadPolly@yandex.ru>
2019-04-16 13:41:00 +0300

    last update
    look up Jenya

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,9 +1,33 @@
 const form = document.querySelector('form')
 
+const switchTo25 = document.querySelector('.switch.is-left')
+const switchTo5 = document.querySelector('.switch.is-right')
+let taskTime = 5000
+
+switchTo25.onclick = function handleTimeSwitch() {
+  if (this.classList.contains('is-active')) {
+    return
+  }
+  this.classList.toggle('is-active')
+  switchTo5.classList.toggle('is-active')
+  taskTime = 5000;
+}
+
+switchTo5.onclick = function handleTimeSwitch() {
+  if (this.classList.contains('is-active')) {
+    return
+  }
+  this.classList.toggle('is-active')
+  switchTo25.classList.toggle('is-active')
+  taskTime = 1000;
+}
+
 form.onsubmit = function startTimer(e) {
   e.preventDefault()
 
   const input = e.target.querySelector('input[type=text]')
 
-  console.log(input.value)
+  setTimeout(function () {
+    console.log(input.value)
+  }, taskTime)
 }
